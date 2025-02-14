3110814aba030dd95add9f798c60c12627727203
m24marketing <sales@m24marketing.com>
2025-02-14 20:50:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use(express.static(__dirname)); // تشغيل ملفات HTML
 // نقطة نهاية لاستقبال بيانات الذكاء الصناعي
 app.post('/api/update-data', (req, res) => {
     const receivedData = req.body;
@@ -13,10 +14,7 @@ app.post('/api/update-data', (req, res) => {
     res.json({
         message: "تم استلام البيانات بنجاح",
         status: "success"
-    });
-});
 
-// تشغيل الخادم
 app.listen(PORT, () => {
     console.log(`🚀 الخادم يعمل على http://localhost:${PORT}`);
 });
